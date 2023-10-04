6892a89cd6fe09fe07beace97d4ffe34c2908236
Ali Abodaraa <abodaraaali50@gmail.com>
2023-10-04 09:38:23 +0300

    use services

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CourseService } from './courses.service';
 @Component({
   selector: 'courses',
   template: `
@@ -12,8 +13,13 @@ import { Component } from '@angular/core';
 })
 export class CoursesComponent {
   title = 'List of course';
+  courses;
+
   getTitle() {
     return this.title;
   }
-  courses = ['course1', 'course2', 'course3'];
+  constructor() {
+    let service = new CourseService(); //instansiation here is tightly coupled whereas in the fucture will get a problem when you decide to add param to this class' constructor then you will need to edit all instansiations statement and anywhere else in your application whrere you have use this service and add a new argument nad that is a fragile
+    this.courses = service.getCourse();
+  }
 }
