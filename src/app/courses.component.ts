a66e9b9b901a652efda2d193ffc1dc66ea9c0596
Ali Abodaraa <abodaraaali50@gmail.com>
2023-10-04 16:22:56 +0300

    event filter

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,30 +1,36 @@
 import { Component } from '@angular/core';
 import { CoursesService } from './courses.service';
+import { NodeEventHandler } from 'rxjs/internal/observable/fromEvent';
 @Component({
   selector: 'courses',
   template: `
-    <h2>{{ title + getTitle() }}</h2>
     <ul>
       <li *ngFor="let course of courses">
         {{ course }}
       </li>
-      <button class="btn btn-danger" [class.active]="isActive">CLICK ME</button>
-      <button
-        class="btn btn-danger"
-        [style.backgroundColor]="isActive ? 'green' : 'red'"
-      >
-        CLICK ME
-      </button>
+      <div>
+        <!-- //traditional way to simulate clicking Enter button -->
+        <label for="traditional way">traditional way</label>
+        <input (keyup)="enterFunTraditional($event)" /><br />
+        <label for="event filter way">event filter way</label>
+        <input (keyup.enter)="enterFunNew()" />
+      </div>
     </ul>
   `,
 })
 export class CoursesComponent {
-  title = 'List of course';
-  isActive = true;
   courses;
 
-  getTitle() {
-    return this.title;
+  enterFun() {
+    console.log('Enter Button Clicked');
+  }
+  enterFunTraditional($event: KeyboardEvent) {
+    if ($event.keyCode == 13)
+      //13 represents Enter key
+      console.log('Enter Button Clicked');
+  }
+  enterFunNew() {
+    console.log('Enter Button Clicked');
   }
   constructor(service: CoursesService) {
     //dependency injection then here when you edit the server constructoe and add a new param will not wdit outside the service class to reflected the change- angular automatically instansiate new CourseService object another benifets to this approch is when you go to unit tests for coursesComponents intead of suppling the accual coursesService to this constructor we can create a fake implementation of this srervice that does not use httpService on the backend
