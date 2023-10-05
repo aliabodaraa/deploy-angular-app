8727fe088c0d3776e53d9fe9ba565b6ab97637a7
Ali Abodaraa <abodaraaali50@gmail.com>
2023-10-05 10:02:04 +0300

    achieve two-way-bnding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [AppComponent, CoursesComponent, CourseComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [CoursesService], //register the dependencies - when register it as a provider in the module angular will create a single instance on that class for this entire module so any- in case we have 100 components and half of them need CoursesService in memory we will have a single instance of CoursesService and Angular will pass the same instance to all these components (Singleton Pattern which means single instance of a given object exists on memory)
   bootstrap: [AppComponent],
 })
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -8,17 +8,18 @@ import { NodeEventHandler } from 'rxjs/internal/observable/fromEvent';
       <li *ngFor="let course of courses">
         {{ course }}
       </li>
-      <input #email (keyup)="tempVarMethod(email.value)" /><br />
+      <input [(ngModel)]="email" (keyup.enter)="tempVarMethod()" /><br />
     </ul>
   `,
 })
 export class CoursesComponent {
   courses;
-
-  tempVarMethod(email: string) {
-    console.log(email);
+  email;
+  tempVarMethod() {
+    console.log(this.email);
   }
   constructor(service: CoursesService) {
+    this.email = 'SDFdsf';
     //dependency injection then here when you edit the server constructoe and add a new param will not wdit outside the service class to reflected the change- angular automatically instansiate new CourseService object another benifets to this approch is when you go to unit tests for coursesComponents intead of suppling the accual coursesService to this constructor we can create a fake implementation of this srervice that does not use httpService on the backend
     this.courses = service.getCourse();
   }
