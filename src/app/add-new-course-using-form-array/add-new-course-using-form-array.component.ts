90ab1c0399bad12a5c8adf9f0d8e2c6d885c9bc3
Ali Abodaraa <abodaraaali50@gmail.com>
2023-10-09 18:40:03 +0300

    difference between the normal approch FormBuilder approch

--- src/app/add-new-course-using-form-array/add-new-course-using-form-array.component.ts
+++ src/app/add-new-course-using-form-array/add-new-course-using-form-array.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import {
   AbstractControl,
   FormArray,
+  FormBuilder,
   FormControl,
   FormGroup,
+  Validators,
 } from '@angular/forms';
 
 @Component({
@@ -26,5 +28,28 @@ export class AddNewCourseUsingFormArrayComponent {
   }
   get topics() {
     return this.form.get('topics') as FormArray<FormControl>; //we cast here because the type that the 'get' method returns is 'AbstractControl' to make access to push we should cast to 'FormArray'
+  }
+  //FormBuilder class users to building forms
+  //difference between the normal approch FormBuilder approch
+  form1;
+  constructor(fb: FormBuilder) {
+    //normal approch
+    this.form1 = new FormGroup({
+      name: new FormControl('', Validators.required),
+      contact: new FormGroup({
+        email: new FormControl(),
+        phone: new FormControl(),
+      }),
+      topics: new FormArray<FormControl>([]),
+    });
+    //FormBuilder approch
+    this.form1 = fb.group({
+      name: ['', Validators.required], //we can call fb.control('', Validators.required) but the witen syntax is much cleaner
+      contact: fb.group({
+        email: [],
+        phone: [],
+      }),
+      topics: fb.array([]),
+    });
   }
 }
